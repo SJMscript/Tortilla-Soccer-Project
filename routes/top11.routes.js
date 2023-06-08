7908738924291ef0dd8ea2bbd1168ea38e53eef9
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-06-08 11:20:19 +0200

    a

--- routes/top11.routes.js
+++ routes/top11.routes.js
@@ -99,88 +99,32 @@ router.post("/createTop11", isAuthenticated, async (req, res, next) => {
     }
 });
 
+// DELETE "/top11/removePlayer/:playerId"
+router.delete("/removePlayer/:playerId", isAuthenticated, async (req, res, next) => {
+  const playerId = req.params.playerId;
+  const userId = req.payload._id;
+
+  try {
+    const removedPlayer = await Top11Player.findOneAndRemove({
+      player: playerId,
+      creator: userId,
+    });
+
+    if (!removedPlayer) {
+      return res.status(404).json({ error: "Player not found in the Top 11" });
+    }
+
+    // Eliminar el jugador del array top11 del usuario
+    await User.findOneAndUpdate(
+      { _id: userId },
+      { $pull: { top11: removedPlayer._id } }
+    );
+
+    res.json(removedPlayer);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
 
-/* const router = require("express").Router();
-const Player = require("../models/Player.model");
-const Top11Player = require("../models/Top11Player.model");
-const User = require("../models/User.model");
-
-// GET "/createTop11"
-router.get("/createTop11", async (req, res, next) => {
-  try {
-    const allPlayers = await Player.find();
-    res.json(allPlayers);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// POST "/createTop11"
-router.post("/createTop11", async (req, res, next) => {
-  const { player, position } = req.body;
-
-  try {
-    const allPlayers = await Top11Player.find();
-    const playerCount = allPlayers.length;
-
-    if (playerCount >= 11) {
-      return res.status(400).json({ error: "Your team has reached 11 players" });
-    }
-
-    let maxGoalkeepers = 1;
-    let maxDefenses = 5;
-    let maxMidfielders = 4;
-    let maxForwards = 3;
-
-    let currentPositionCount = 0;
-    let isPlayerInTeam = false;
-
-    for (let i = 0; i < allPlayers.length; i++) {
-      if (allPlayers[i].position === position) {
-        currentPositionCount++;
-      }
-
-      if (allPlayers[i].player.toString() === player) {
-        isPlayerInTeam = true;
-        break;
-      }
-    }
-
-    if (isPlayerInTeam) {
-      return res.status(400).json({ error: "The player is already in the team" });
-    }
-
-    switch (position) {
-      case "goalkeeper":
-        if (currentPositionCount >= maxGoalkeepers) {
-          return res.status(400).json({ error: "The maximum number of goalkeepers has been reached" });
-        }
-        break;
-      case "defense":
-        if (currentPositionCount >= maxDefenses) {
-          return res.status(400).json({ error: "The maximum number of defenders has been reached" });
-        }
-        break;
-      case "midfielder":
-        if (currentPositionCount >= maxMidfielders) {
-          return res.status(400).json({ error: "The maximum number of midfielders has been reached" });
-        }
-        break;
-      case "forward":
-        if (currentPositionCount >= maxForwards) {
-          return res.status(400).json({ error: "The maximum number of forwards has been reached" });
-        }
-        break;
-      default:
-        return res.status(400).json({ error: "Invalid position" });
-    }
-
-    const newTop11Player = await Top11Player.create({ player, position });
-    res.json(newTop11Player);
-  } catch (error) {
-    next(error);
-  }
-});
-
-module.exports = router; */
